c23a5dadde676240490b0d7d797d7010fe26ac68
Aswath S <aswath.18cs@kct.ac.in>
2022-03-07 01:13:13 +0530

    made the tests to work and rectified naming conventions

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -1,11 +1,11 @@
 package powerpackage;
 
 public class powerfinder {
-    public static int OF(int xyz, int pqr) {
-        int p = 1;
-        for (int i = 0; i<pqr; i++) {
-            p *= xyz;
+    public static int OF(int base, int exponent) {
+        int acc = 1;
+        for (int i = 0; i < exponent; i++) {
+            acc *= base;
         }
-        return p;
+        return acc;
     }
 }
--- src/test/powertest.java
+++ src/test/powertest.java
@@ -1,26 +1,25 @@
 package powerpackage;
 
-
 import org.junit.Test;
 
 public class powertest {
     @Test
-    public void one_raised_to_one_is_onew() {
-        assert powerfinder.OF(1,1) == 1;
+    public void one_raised_to_one_is_one() {
+        assert powerfinder.OF(1, 1) == 1;
     }
 
     @Test
-    public void twoRaised_to_one_is_2() {
-        assert powerfinder.OF(2,1) == 2;
+    public void two_raised_to_one_is_two() {
+        assert powerfinder.OF(2, 1) == 2;
     }
 
     @Test
-    public void two_power_2_is_4() {
+    public void two_raised_to_two_is_four() {
         assert powerfinder.OF(2, 2) == 4;
     }
 
     @Test
-    public void power_of_2_and_3_Is_SIX() {
-        assert powerfinder.OF(3, 2) == 3*3;
+    public void three_raised_to_two_is_nine() {
+        assert powerfinder.OF(3, 2) == 3 * 3;
     }
 }
